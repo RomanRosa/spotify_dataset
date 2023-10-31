aa099326103e0346dc714be2aecdc5637d3a531f
Roman de la Rosa <mldevops2@gmail.com>
2023-10-30 18:40:58 -0600

    rename columns based their variable type

--- main.py
+++ main.py
@@ -35,3 +35,19 @@ c_feats = ['acousticness','danceability','duration_ms','energy',
 v_feats = ['mode','key','explicit','popularity','genero']
 t_feats = ['artists','id','name','zip Code']
 d_feats = ['release_date']
+
+c_feats_new = ['c_' + x for x in c_feats]
+v_feats_new = ['v_' + x for x in v_feats]
+d_feats_new = ['d_' + x for x in d_feats]
+t_feats_new = ['t_' + x for x in t_feats]
+
+print(list(c_feats))
+print(list(c_feats_new))
+
+# Rename columns according to the type of variable
+spotify.rename(columns=dict(zip(d_feats,d_feats_new)),inplace=True)
+spotify.rename(columns=dict(zip(v_feats,v_feats_new)),inplace=True)
+spotify.rename(columns=dict(zip(t_feats,t_feats_new)),inplace=True)
+spotify.rename(columns=dict(zip(c_feats,c_feats_new)),inplace=True)
+print(spotify.shape)
+print(spotify.head())
